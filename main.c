643d60adafbaefa0b558d8a3cfac0b2139b536d3
the-benbart <benbart@benbart.net>
2015-07-21 16:51:05 +0200

    1ere fonction en teste

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-taaaqqq	zduzudhu
+#include <stdio.h>
+#include <hardware-drive.h>
+
+int main (void)
+{
+   printLCD ("Hello, Wait for initialisation");
+   printLCD ("Salut, Patientez pendant l initialisation");
+
+   return 0;
+}  
+
