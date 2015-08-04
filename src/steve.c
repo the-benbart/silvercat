2358461d072d94a110f569b7497bff3b6cf6cdbd
the-benbart <benbart@benbart.net>
2015-08-04 16:48:56 +0200

    revision steve.c

--- src/steve.c
+++ src/steve.c
@@ -1,20 +1,92 @@
 /* main script for drive Steve protocol */
+#include <unistd.h>
+#include <stdio.h>
 #include "motors.h"
 #include "lcd.h"
 #include "core.h"
-void steve( void )
-{
-  do
-  {
-    takeToken(1); // Take the Token / If 0=return token / 1=Got Token
-    sreen(1);
-    if (captorUp = 0) // calibrage
-    {
-      motorControl(1); // 0 = Idle 1=To UP 2= To Down
-    }                 // End calibrage
-                      //Down
-  }
-while( stop == 0);
 
-return 0;
+// Set token system & steve running job
+int token = 1;
+int calibrage;
+int down;
+int up;
+int hold;
+int alert;
+
+int steve (void)
+{
+  screen(calibrageStart)
+/////////////////////////////////////////////////////
+
+//                calibrage stage on               //
+
+/////////////////////////////////////////////////////
+  while ( captorUP != 1 || token == 1 )
+  {
+    screen(calibrageRUN)
+    token = 0
+    gearsUP(struct config(calibrageSpeed))
+    token = 1
+  }
+  screen(calibrageStop)
+
+  /////////////////////////////////////////////////////
+
+  //                calibrage stage off              //
+
+  /////////////////////////////////////////////////////
+
+  //                Peigne gearsDown on              //
+
+  /////////////////////////////////////////////////////
+  screen(downStart)
+  while ( captorDown != 1 || token == 1 )
+  {
+    token = 0
+    screen(downRUN)
+    gearsDown(struct config(downSpeed))
+    token = 1
+  }
+  screen(downStop)
+  /////////////////////////////////////////////////////
+
+  //                Peigne gearsDown off             //
+
+  /////////////////////////////////////////////////////
+
+  //                Peigne gearsHold on              //
+
+  /////////////////////////////////////////////////////
+  screen(holdStart)
+  while (captorDown == 1 || token == 1)
+  {
+    token = 0
+    screen(holdRUN)
+    sleep (struct config(holdSleep))
+    token = 1
+  }
+  screen(holdStop)
+  /////////////////////////////////////////////////////
+
+  //                Peigne gearsHold off             //
+
+  /////////////////////////////////////////////////////
+
+  //                Peigne gearsUP on                //
+
+  /////////////////////////////////////////////////////
+  screen(upStart)
+  while ( captorUp != 1 || token == 1 )
+  {
+    token = 0
+    screen(upRUN)
+    gearsUp(struct config(upSpeed))
+    token = 1
+  }
+  screen(upStop)
 }
+/////////////////////////////////////////////////////
+
+//                Peigne gearsUP off               //
+
+/////////////////////////////////////////////////////
