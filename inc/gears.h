0903bb92c29477f54889f0e4bfbd25e44dfa3b78
the-benbart <benbart@benbart.net>
2015-08-05 16:48:31 +0200

    struct config {} | add comment | rectifications

--- inc/gears.h
+++ inc/gears.h
@@ -8,11 +8,23 @@ function doFinish
 function calibrage
 */
 
-/* Motor Control */
-gearsDown();
-gearsUp();
+/////////////////////////////////////////////////////
 
-/* LCD Control */
+//                motor control               //
+
+/////////////////////////////////////////////////////
+
+
+gearsDown(); // function for climb the platform
+gearsUp(); // function for descend the platform
+
+
+
+/////////////////////////////////////////////////////
+
+//                LCD control                      //
+
+/////////////////////////////////////////////////////
 
 int screen(lcdScreen){
   switch (lcdScreen)
@@ -120,3 +132,12 @@ int screen(lcdScreen){
     break;
     }
   }
+
+struct config // Core configuration, PLEASE DON'T TOUCH
+{
+  int calibrageSpeed[255]; // 0-255 speed to Calibrate, DO NOT TOUCH
+  int downSpeed[255]; // 0-255 speed to DOWN, DO NOT TOUCH
+  int holdSleep[255]; // Time to incubate, DO NOT TOUCH
+  int upSpeed[255]; // 0-255 speed to UP, DO NOT TOUCH
+  int alertMod[2]; // 0=No Alert / 1 = Few 3 bip alert / 2 = One bip every 5 seconds
+}
